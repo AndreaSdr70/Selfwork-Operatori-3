32dc33b147768666bbaffd156969fbe41a61ef8d
AndreaSdr70 <andrea@sdringola.it>
2024-07-11 01:09:50 +0200

    Selfwork Operatori 3

---
+++ main.js
@@ -0,0 +1,10 @@
+let n1 = prompt(" Inserisci il primo numero ");
+let n2 = prompt(" Inserisci il secondo numero ");
+
+let somma = n1 + n2;
+let sottrazione = n1 - n2;
+let moltiplicazione = n1 * n2;
+let divisione = n1 / n2;
+let potenza = n1 ** n2;
+
+console.log("Con i numeri da te scelti, i risultati delle varie operazioni sono: somma " + somma + ", sottrazione " + sottrazione + ", moltiplicazione "+ moltiplicazione + ", divisione " + divisione + ", potenza " + potenza);
